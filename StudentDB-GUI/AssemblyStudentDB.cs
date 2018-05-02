d4ea4c871bdbbc2b0e05e77b94935de0906eecad
khaled-hamam <Khal3d.Mohamed@gmail.com>
2018-05-02 04:37:45 +0200

    Assembly Code Integration

---
+++ StudentDB-GUI/AssemblyStudentDB.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+
+namespace StudentDB_GUI
+{
+    class AssemblyStudentDB
+    {
+        const string StudentDBdll = "StudentDB.dll";
+
+        [DllImport(StudentDBdll)]
+        public static extern void enrollStudent(char id, string name, char nameSize, char grade, char section);
+
+        [DllImport(StudentDBdll)]
+        public static extern void saveDatabase(string FName, char KEY);
+
+        [DllImport(StudentDBdll)]
+        public static extern void openDatabase(string FName, char KEY);
+
+        [DllImport(StudentDBdll)]
+        public static extern void updateGrade(char studentID, char newGrade);
+
+        [DllImport(StudentDBdll)]
+        public static extern void deleteStudent(char studentID);
+
+        [DllImport(StudentDBdll)]
+        public static extern void printStudent(char studentID, [Out] char[] studentData);
+
+        [DllImport(StudentDBdll)]
+        public static extern void generateSectionReport(char sectionNumber, string fileName);
+
+        [DllImport(StudentDBdll)]
+        public static extern void top5Students([Out] char[] studentsData);
+
+        [DllImport(StudentDBdll)]
+        public static extern void printStudentsBuffer([Out] char[] studentsBuffer);
+    }
+}
