9b80a4affdbe5511fb98c94121bc2a7f034b0a59
khaled-hamam <Khal3d.Mohamed@gmail.com>
2018-05-02 04:38:05 +0200

    Student Model Class

---
+++ StudentDB-GUI/StudentModel.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentDB_GUI
+{
+    class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int NumericGrade { get; set; }
+        public char AlphabeticGrade { get; set; }
+        public int Section { get; set; }
+    }
+}
